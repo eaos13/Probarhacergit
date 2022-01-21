4a2e6b85d12c40b9f13782f79c64fd19be71dbc8
casarudin <casarudin@EDWINOS>
2022-01-20 22:30:41 -0600

    Agregar archivos de proyecto.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿/*Mi primer programa en C++  */
+
+#include "iostream"
+#include "string"
+
+
+using namespace std;
+
+int main()
+{
+    cout << "\t\t\t" << endl;
+    cout << "\t\t\t Soy un gran programador en varios Lenguajes 2022 " << endl;
+    cout << "\t\t\t";
+
+    cout << endl << endl;
+    system("pause");
+    return 0;
+}
